3d0e1f3bd18f07b15222025b0a2b1ceb882b05b7
Jatin Panchal <jatin.panchal@piri.ai>
2020-04-27 01:39:21 +0530

    fix in starup.cs file

--- HelloWorld/Startup.cs
+++ HelloWorld/Startup.cs
@@ -8,9 +8,11 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+
 
 namespace HelloWorld
-{
+{    
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -21,14 +23,23 @@ namespace HelloWorld
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+	    
+    	    services.Configure<ForwardedHeadersOptions>(options =>
+    		{
+        	options.ForwardedHeaders = 
+            	    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    		});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+	    app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -38,13 +49,9 @@ namespace HelloWorld
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+		app.UseAuthentication();
             }
-
-	    app.UseForwardedHeaders(new ForwardedHeadersOptions
-	    {
-		ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-	     });
-
+    
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
